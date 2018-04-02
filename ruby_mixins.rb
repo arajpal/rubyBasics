4620af22f8262cd58f8d0f2b2a0d856912b586ac
abhay <arajpal@bestpeers.com>
2018-04-02 20:23:20 +0530

    call instance and class method by module in a class

---
+++ ruby_mixins.rb
@@ -0,0 +1,32 @@
+$movie_id = 0#global variable
+module Pvr
+  def self.included(films)
+    films.extend(Inox)
+  end
+  module Inox
+    def id#class method
+      $movie_id = $movie_id + 1
+    end
+  end
+  def movie_type#instance method
+    puts "enter the type of movie you are watching"
+    @movie_type = gets
+  end
+end
+class Movies
+  def movie_name
+    puts "enter name of movie"
+    @movie_name = gets#instance variable
+  end
+  include Pvr
+  def movie_output
+    puts "Movie-type : #{@movie_type}"
+    puts "Movie-name : #{@movie_name}"
+    puts "Number of movie you have watched : #{$movie_id}"
+  end
+end
+movie = Movies.new
+movie.movie_name
+movie.movie_type
+Movies.id
+movie.movie_output
